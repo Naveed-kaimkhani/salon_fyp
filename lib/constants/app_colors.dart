36bfc3b944b8c0d3d3633d963ec558b1c79bfe48
Naveed-kk <naveedkaimkhami@gmail.com>
2025-04-19 03:41:50 -0700

    .

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static List<Color> appGradientColors = [
     purple,
-    const Color(0xFFB263E0),
+    Colors.pink,
   ];
 
   static const grey = Color(0xff474747);
@@ -12,7 +12,7 @@ class AppColors {
   static const black = Color(0xff000000);
   static const blue = Color(0xff235AFF);
   static const white = Color(0xffffffff);
-  static const purple = Color(0xFF896ED9);
+  static const purple = Color(0xFFB63889);
   static const green = Color(0xff34A853);
   static const red = Colors.red;
 }
